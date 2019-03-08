90e19fdba482c046e87f8a1b18ede6dfda18ce29
logalex96 <logalex96@gmail.com>
2019-03-08 13:21:25 +0200

    final_lab2

---
+++ lab2/Calculator/Calculator/Models/Calculator.cs
@@ -0,0 +1,67 @@
+﻿namespace Calculator.Models
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Calculator
+    {
+        private static bool _operandSelected;
+
+        public static readonly string[] OperatorsSymbols = { "+", "-", "*", "/" };
+
+        public static Dictionary<string, string> Operation { get; } = 
+            new Dictionary<string, string>()
+                {
+                    { "Plus", "+" },
+                    { "Minus", "-" },
+                    { "Multiplication", "*" },
+                    { "Division", "/" }
+                };
+
+        public static Dictionary<string, bool> Type { get; } = 
+            new Dictionary<string, bool>()
+                {
+                    { "Float", false }, 
+                    { "PositiveSign", true }
+                };
+
+        public int LeftParenthesis { get; set; }
+
+        public Calculator()
+        {
+            LeftParenthesis = 0;
+        }
+
+        public static bool TryGetOperationKey(string value, out string key)
+        {
+            try
+            {
+                var operationKey = Operation.Where(item => item.Value.Equals(value))
+                    .Select(item => item.Key);
+                key = operationKey.FirstOrDefault();
+            }
+            catch (Exception)
+            {
+                key = string.Empty;
+                return false;
+            }
+            return true;
+        }
+        public bool HasOperand()
+        {
+            return _operandSelected;
+        }
+
+        public void NumericSelection()
+        {
+            _operandSelected = false;
+        }
+
+        public void ResetCalculatorBuff()
+        {
+            _operandSelected = false;
+            LeftParenthesis = 0;
+        }
+    }
+}
